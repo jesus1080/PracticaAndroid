2a60483c0f48af649b4f390d5ef1854030232f8c
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-09-21 16:36:30 -0500

    feat: add composable functions

--- app/src/main/java/com/example/presentacion/MainActivity.kt
+++ app/src/main/java/com/example/presentacion/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.presentacion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,23 +12,57 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.presentacion.ui.theme.PresentacionTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PresentacionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Tarjeta()
         }
     }
 }
+
+@Composable
+fun Tarjeta(){
+    Column {
+        ImagePre()
+        Datos(name = "Jesus Ruiz Velasco", ocupacion = "Cientifico de Datos")
+        Contacto(name = "Cualesquira")
+    }
+}
+@Composable
+fun ImagePre() {
+    Image(
+        painter = painterResource(R.drawable.otra),
+        contentDescription = "Contact profile picture",
+    )
+}
+
+@Composable
+fun Datos(name: String, ocupacion: String) {
+    Column() {
+        Text(name)
+        Text(ocupacion)
+    }
+
+}
+@Composable
+fun Contacto(name: String) {
+    Text(text = "Hello $name!")
+}
+
+
+
+
+
+
+
+
+
 
 @Composable
 fun Greeting(name: String) {
