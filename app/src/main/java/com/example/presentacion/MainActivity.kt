e58bf2138f42ae63e6f252b3ec3d18820bf0d0e1
Jesus Ruiz <jesusruiz@unicauca.edu.co>
2023-09-21 20:40:37 -0500

    feat: add icons

--- app/src/main/java/com/example/presentacion/MainActivity.kt
+++ app/src/main/java/com/example/presentacion/MainActivity.kt
@@ -11,12 +11,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.presentacion.ui.theme.PresentacionTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
 
 
 class MainActivity : ComponentActivity() {
@@ -62,16 +69,46 @@ fun Datos(name: String, ocupacion: String) {
 @Composable
 fun Contacto() {
     Column(
-        modifier = Modifier.size(200.dp).offset(x=90.dp,y=630.dp),
+        modifier = Modifier
+            .size(200.dp)
+            .offset(x = 95.dp, y = 630.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
-        Text( "Telefono")
-        Text( "Correo")
-        Text( "Redes!")
+        Telefono(item = "33455666")
+        Correo(item = "pepito@mail.com")
+        Redes(item = "@pepito22")
     }
 }
 
+@Composable
+fun Telefono(item:String){
+    Row(){
+        Icon(imageVector = Icons.Default.Phone, contentDescription = item,modifier = Modifier.padding(8.dp))
+        Text(text = item)
+    }
+    
+}
+@Composable
+fun Correo(item:String){
+    Row(){
+        Icon(imageVector = Icons.Default.Email,
+            contentDescription = item,
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(text = item)
+    }
+
+}
+
+@Composable
+fun Redes(item:String){
+    Row(){
+        Icon(imageVector = Icons.Default.Share, contentDescription = item,modifier = Modifier.padding(8.dp))
+        Text(text = item)
+    }
+
+}
 
 
 
